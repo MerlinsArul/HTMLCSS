a91b92ce023fb4e00c81b24df1e4eb4ac034d4c8
ASPLAP3254 <merlins.arul@aspiresys.com>
2022-11-24 09:00:42 +0530

    adding all files

---
+++ clgscript.js
@@ -0,0 +1,20 @@
+
+
+function mouseOver() {
+    document.getElementById("style").style.color = "blue";
+}
+function mouseOut() {
+    document.getElementById("style").style.color = "black";
+}
+function func(x) {
+    document.getElementById(x).style.background = "pink";
+}
+function funct(x) {
+    document.getElementById(x).style.background = "white";
+}
+function funct1() {
+    alert("Your comment submited Successfully")
+}
+function funct2() {
+    alert("Your message hes been  sent")
+}
